41d1e5b8ebbcdcdfb87b3f4b04f9db1a28022ef9
christian dungca <cdungca14@gmail.com>
2020-04-10 13:27:51 +0100

    created a function to get data from and api, another function to act as a middle man, another function to receive that data and use it. Using the pixabay api to get photo urls and then use that in the application. Created the first section of the page, which contains a main post and several sub post, recreating layout from dribble

--- jsonPlaceholder/displayingData/app.js
+++ jsonPlaceholder/displayingData/app.js
@@ -1,29 +1,48 @@
-console.log('app is running');
 
+/* example get request https://pixabay.com/api/?key=10508573-91c4fd6a2f0fe907b6329f306&q=yellow+flowers&image_type=photo */
+
+const apiKey = '10508573-91c4fd6a2f0fe907b6329f306';
+
+const DOM = {
+    'headerPhoto' : document.querySelector('.header .header__main-photo'),
+}
 
 const getPhotos = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/photos';
+    const url = `https://pixabay.com/api/?key=${apiKey}&image_type=photo`;
 
     try {
         const res = await fetch(url);
         const data = await res.json();
 
-        console.log(data);
-        return data;
-    } catch(e) {
-        console.log('ERROR', e);
+        const imagesUrl = data.hits.map((image) => {
+            return image.webformatURL
+        });
+    
+        return imagesUrl
+    } catch (error) {
+        console.log(error)
+    } 
+}
+
+const photoHandler = async () => {
+    try {
+        const photosList = await getPhotos()
+        createImage(photosList[0]);
+    } catch (e) {
+        console.log(e);
     }
-} 
+}
+photoHandler();
 
-getPhotos();
+const createImage = (imageUrl) => {
+    const mainPhoto = DOM.headerPhoto;
 
-const getPosts = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const imageToInsert = `<img src="${imageUrl}" alt="photo of something">`
 
-    const res = await fetch(url);
-    const data = await res.json();
+    mainPhoto.insertAdjacentHTML('afterbegin', imageToInsert);
 
-    console.log('DATA:', data);
-}   
+    console.log('done:', mainPhoto);
+}
 
-// getPosts();
+
+console.log(DOM.headerPhoto);
